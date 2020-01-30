a7ce608b23d5743bc0d0046eb3782d09f3f279fb
ilabstaff <ilabstaff@172.16.116.128>
2020-01-29 18:05:34 -0800

    finishing_up_for_loop_gauntlet

--- src/_06_for_loop_gauntlet/for_loop_gauntlet.java
+++ src/_06_for_loop_gauntlet/for_loop_gauntlet.java
@@ -4,16 +4,20 @@ import javax.swing.JOptionPane;
 
 public class for_loop_gauntlet {
 	public static void main(String[] args) {
-	int age;
-	for (int i =2004 ; i <2020; i++) {
-		age=i-2004;
+	//int age;
+	for (int i =0 ; i <7; i+=3) {
+		for(int j=1;j<4;j++) {
+		System.out.print(j+i);
 		
+	
+			}
 	//	if (i%7==0)
-		System.out.println("in "+i+",i was "+ age+" years old");
+		//System.out.println("in "+i+",i was "+ age+" years old");
 		
+		System.out.println();
 		
 	}
-		
+
 	}
 
 }
