b1932da8f8aa00118a92a46823d4f812cfc7fcc1
ilabstaff <ilabstaff@172.16.116.79>
2020-02-19 18:02:00 -0800

    working_on_prime_number

--- src/_01_prime_or_not/prime.java
+++ src/_01_prime_or_not/prime.java
@@ -3,16 +3,21 @@ package _01_prime_or_not;
 import javax.swing.JOptionPane;
 
 public class prime {
-public static void main(String[] args) {
-	
- String input=JOptionPane.showInputDialog("Pick a number");
-int num=Integer.parseInt(input);
-for(int i=2;i<num;i++) {
-if	(num%i==0)
-	JOptionPane.showMessageDialog(null, "not prime");
+	public static void main(String[] args) {
+
+		String input = JOptionPane.showInputDialog("Pick a number");
+		int num = Integer.parseInt(input);
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println( i +" is not prime");
+				
+			}
+			else
+				System.out.println("i is " + i);
+				System.out.println( i + " is prime");
+		}
+
+	}
 
 }
-JOptionPane.showMessageDialog(parentComponent, message);
-}
-	
-}
+  
